feaa024ca0486ea06f966ca25f453d043ab4967b
DavidKendall <david.kendall@northumbria.ac.uk>
2015-10-19 11:53:50 +0100

    Report sensor values on display

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,25 @@
 #include <mbed.h>
 #include <MMA7455.h>
 #include <LM75B.h>
+#include <display.h>
 
 bool accInit(MMA7455& acc);
+Display *d = Display::theDisplay();
 
 int main() {
   DigitalOut led1(LED1);
 	AnalogIn pot(p15);
 	MMA7455 acc(P0_27, P0_28);
   LM75B lm75b(P0_27, P0_28, LM75B::ADDRESS_1);
-	Serial s(USBTX, USBRX);
 	float potVal = 0.0;
 	float tempVal = 0.0;
 	int32_t accVal[3];
 	
-	s.baud(115200);
-	printf("Hello World\n");
+	/* Initialise the display */	
+	d->fillScreen(WHITE);
+	d->setTextColor(BLACK, WHITE);
+
+	/* Initialise accelerometer and temperature sensor */
 	accInit(acc);
 	lm75b.open();
 	while (true) {
@@ -26,23 +30,26 @@ int main() {
 		potVal = pot.read();
 		tempVal = lm75b.temp();
 		acc.read(accVal[0], accVal[1], accVal[2]);
-		printf("Pot  : %1.2f\n", potVal);
-		printf("Temp : %03.1f\n", tempVal);
-		printf("Acc  : %05d, %05d, %05d\n", accVal[0], accVal[1], accVal[2]); 
+		d->setCursor(2,2);
+		d->printf("Pot  : %1.2f\n", potVal);
+		d->setCursor(2,12);
+		d->printf("Temp : %03.1f\n", tempVal);
+		d->setCursor(2,22);
+		d->printf("Acc  : %05d, %05d, %05d\n", accVal[0], accVal[1], accVal[2]); 
 	}
 }
 
 bool accInit(MMA7455& acc) {
 	bool result = true;
   if (!acc.setMode(MMA7455::ModeMeasurement)) {
-    printf("Unable to set mode for MMA7455!\n");
+    // d->printf("Unable to set mode for MMA7455!\n");
     result = false;
   }
 	if (!acc.calibrate()) {
-    printf("Failed to calibrate MMA7455!\n");
+    // d->printf("Failed to calibrate MMA7455!\n");
     result = false;
   }
-	printf("MMA7455 initialised\n");
+	// d->printf("MMA7455 initialised\n");
   return result;
 }
 
